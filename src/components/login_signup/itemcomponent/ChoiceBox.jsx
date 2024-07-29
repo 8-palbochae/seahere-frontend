6005c801207ffdc5401ec68d3b7a398faedde3f2
kimdodo97 <eh4536@naver.com>
2024-07-29 13:36:32 +0900

    Feat : 회원 가입 종류에 따른 전역 상태 관리 추가

--- src/components/login_signup/itemcomponent/ChoiceBox.jsx
+++ src/components/login_signup/itemcomponent/ChoiceBox.jsx
@@ -1,10 +1,18 @@
 // SignUpOption.jsx
 import { Link } from "react-router-dom";
+import useUserTypeStore from '../../../stores/signupType';
 
-const ChoiceBox = ({ to, children }) => {
+const ChoiceBox = ({ to, onClick , children }) => {
+    const handleClick = (event) => {
+        if (onClick) {
+          onClick(); // 클릭 핸들러 호출
+        }
+      };
+
     return (
         <Link to={to}>
-            <div className="flex items-center justify-center w-[316px] h-[160px] bg-[#fffffff2] rounded-[25px] border border-solid border-[#d9d9d9]">
+            <div className="flex items-center justify-center w-[316px] h-[160px] bg-[#fffffff2] rounded-[25px] border border-solid border-[#d9d9d9]"
+            onClick={handleClick}>
                 <div className="[font-family: 'inter-Regular', Helvetica] font-normal text-black text-center tracking-[0] leading-[30px] whitespace-nowrap">
                     {children}
                 </div>
