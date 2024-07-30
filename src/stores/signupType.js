82c6d77bc4e20928ae423eefbbe2d36b50278d09
kimdodo97 <eh4536@naver.com>
2024-07-30 10:39:40 +0900

    Feat : 회사 정보 전역 상태 관리  추가

--- src/stores/signupType.js
+++ src/stores/signupType.js
@@ -4,7 +4,9 @@ import { persist } from 'zustand/middleware';
 const useUserTypeStore = create(persist(
   (set) => ({
     userType: null, // 초기 상태
+    companyId: null, // 초기 상태
     setUserType: (type) => set({ userType: type }), // 상태 업데이트 함수
+    setCompanyId: (id) => set({ companyId: id }), // 회사 ID 상태 업데이트 함수
   }),
   {
     name: 'user-type-session-storage', // 세션 저장소에 저장될 키 이름
