8197cf4f8b7c56c3c30040853e34c3e080459655
kimdodo97 <eh4536@naver.com>
2024-07-30 17:50:41 +0900

    Feat : 회워 가입 전역 상태 관리 추가

--- src/stores/signupType.js
+++ src/stores/signupType.js
@@ -3,10 +3,14 @@ import { persist } from 'zustand/middleware';
 
 const useUserTypeStore = create(persist(
   (set) => ({
-    userType: null, // 초기 상태
-    companyId: null, // 초기 상태
+    userType: null, 
+    companyId: null,
+    guestId : null, 
     setUserType: (type) => set({ userType: type }), // 상태 업데이트 함수
-    setCompanyId: (id) => set({ companyId: id }), // 회사 ID 상태 업데이트 함수
+    setCompanyId: (id) => set({ companyId: id }), 
+    setGuestId: (id) => set({guestId : id}),
+    initializeState: (userType = null, companyId = null, guestId = null) => 
+      set({ userType, companyId, guestId }),
   }),
   {
     name: 'user-type-session-storage', // 세션 저장소에 저장될 키 이름
