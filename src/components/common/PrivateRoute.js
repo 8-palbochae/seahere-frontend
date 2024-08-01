2e1df8f8f1b14c398bc0de38e9d134c0536c3589
kimdodo97 <eh4536@naver.com>
2024-08-01 12:51:16 +0900

    Fix : 로그인 상태에 리프레시 토큰 추가

--- src/components/common/PrivateRoute.js
+++ src/components/common/PrivateRoute.js
@@ -4,14 +4,12 @@ import { Navigate, Outlet } from 'react-router-dom';
 import { useAuthenticationStore } from '../../stores/authentication';
 
 const PrivateRoute = () => {
-  const { accessToken } = useAuthenticationStore();
+  const { accessToken , refreshToken} = useAuthenticationStore();
 
-  if (!accessToken) {
-    // 인증되지 않은 경우 로그인 페이지로 리다이렉트
+  if (!accessToken && !refreshToken) {
     return <Navigate to="/login" replace />;
   }
 
-  // 인증된 경우 자식 컴포넌트 렌더링
   return <Outlet />;
 };
 
