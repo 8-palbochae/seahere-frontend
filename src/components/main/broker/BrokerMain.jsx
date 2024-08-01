1c35ae656714bba67b42ca7212ed81bab99cbbdd
kimdodo97 <eh4536@naver.com>
2024-08-01 13:39:23 +0900

    Remove : 토큰 UseEffect 삭제

--- src/components/main/broker/BrokerMain.jsx
+++ src/components/main/broker/BrokerMain.jsx
@@ -11,23 +11,6 @@ import { useAuthenticationStore } from '../../../stores/authentication';
 const BrokerMain = () => {
 	const {accessToken,refreshToken,setAccessToken, setRefreshToken, deleteCookie } = useAuthenticationStore();
 
-	useEffect(() => {
-    const fetchTokens = async () => {
-			if (accessToken === null && refreshToken === null) {
-				try {
-					const [access, refresh] = await authenticationGet();
-					setAccessToken(access);
-					setRefreshToken(refresh);
-				} catch (error) {
-					console.error("Failed to fetch tokens:", error);
-				}
-			}
-		};
-
-		fetchTokens();
-
-	}, [accessToken,refreshToken,setAccessToken, setRefreshToken]);
-	
 	return (
 		<div className="flex flex-col items-center w-full gap-3">
 			<div className="p-2 rounded-[20px] w-full h-1/5">
