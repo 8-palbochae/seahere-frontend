de6dbfa144bd5d8bb0682968905c75320497fecd
DOYOUNG KIM <82021950+kimdodo97@users.noreply.github.com>
2024-08-02 10:21:34 +0900

    Feat ; 로그인 시 로그아웃 기능 추가 (#32)

--- src/constants/header/header.image.js
+++ src/constants/header/header.image.js
@@ -1,8 +1,10 @@
 import bellIcon from "../../assets/header/bell-icon.svg";
 import boxIcon from "../../assets/header/box-icon.svg";
+import logoutIcon from "../../assets/header/logout-icon.svg"
 const headerIcon = {
 	bellIcon,
 	boxIcon,
+	logoutIcon,
 };
 
 export default headerIcon;
