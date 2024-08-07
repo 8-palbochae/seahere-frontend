a6302cefe00a7675b21b4f8bdebc944f1afa5f95
jang643 <jang643@naver.com>
2024-08-07 21:11:03 +0900

    목데이터 제거

--- src/components/inventory/MainInventory.jsx
+++ src/components/inventory/MainInventory.jsx
@@ -7,9 +7,9 @@ const MainInventory = () => {
 
   const { setHeaderText } = useHeaderText();
 
-    useEffect(() => {
-        setHeaderText("재고 확인");
-    }, [setHeaderText]);
+  useEffect(() => {
+    setHeaderText("재고 확인");
+  }, [setHeaderText]);
 
   const [searchOption, setSearchOption] = useState("");
   const [size, setSize] = useState(calculateSize(window.innerHeight));
@@ -43,7 +43,6 @@ const MainInventory = () => {
       </div>
       <div className="flex-1 overflow-y-auto w-full mt-4">
         <InventoryList
-          companyId={101}
           size={size}
           searchOption={searchOption}
         />
