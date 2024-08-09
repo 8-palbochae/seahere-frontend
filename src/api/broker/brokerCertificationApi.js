88e87a528105d19a56a1b2c37ab6cf8cb531c0c6
Jeong gi seok <wjdrltjr5@naver.com>
2024-08-09 16:17:57 +0900

    Feat : http -> https

--- src/api/broker/brokerCertificationApi.js
+++ src/api/broker/brokerCertificationApi.js
@@ -1,36 +1,37 @@
 import axios from "axios";
 
 export const certifyCompany = async (regNumber, ceoName, startDate) => {
-    const body = {
-        "businesses": [
-            {
-                "b_no": `${regNumber}`,
-                "start_dt": `${startDate}`,
-                "p_nm": `${ceoName}`,
-            }
-        ]
-    };
+	const body = {
+		businesses: [
+			{
+				b_no: `${regNumber}`,
+				start_dt: `${startDate}`,
+				p_nm: `${ceoName}`,
+			},
+		],
+	};
 
-    const serviceKey = "w%2F9mQZHK9naA0VQgcCPay%2B%2BAFoyLPVRNclaFvbIOC8%2B8BPTu%2FTAt1K9EdqkqnUlyJx4GEXMD4tsK22jqm7NLuA%3D%3D";
+	const serviceKey =
+		"w%2F9mQZHK9naA0VQgcCPay%2B%2BAFoyLPVRNclaFvbIOC8%2B8BPTu%2FTAt1K9EdqkqnUlyJx4GEXMD4tsK22jqm7NLuA%3D%3D";
 
-    try {
-        const response = await axios.post(
-            `http://api.odcloud.kr/api/nts-businessman/v1/validate?serviceKey=${serviceKey}`,
-            body,
-            {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json'
-                }
-            }
-        );
-        if (response.data.data[0].valid === '01') {
-            return true;
-        } else {
-            return false;
-        }
-    } catch (error) {
-        console.log(error)
-        return false; // 에러 발생 시 false 반환
-    }
+	try {
+		const response = await axios.post(
+			`https://api.odcloud.kr/api/nts-businessman/v1/validate?serviceKey=${serviceKey}`,
+			body,
+			{
+				headers: {
+					"Content-Type": "application/json",
+					Accept: "application/json",
+				},
+			}
+		);
+		if (response.data.data[0].valid === "01") {
+			return true;
+		} else {
+			return false;
+		}
+	} catch (error) {
+		console.log(error);
+		return false; // 에러 발생 시 false 반환
+	}
 };
