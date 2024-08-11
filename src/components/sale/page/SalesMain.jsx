373c5fc9d1671d169dc2f3793044e74f649f109d
woneveryady <woneveryday@naver.com>
2024-08-11 13:40:13 +0900

    헤더 수정

--- src/components/sale/page/SalesMain.jsx
+++ src/components/sale/page/SalesMain.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React,{useEffect} from "react";
 import IncomingSales from "../itemcomponent/IncomingSales";
+import { useHeaderText } from "../../../stores/headerText";
 
 const SalesMain = () => {
+    const {setHeaderText} = useHeaderText();
+
+    useEffect(() => {
+        setHeaderText("매출 확인");
+      }, [setHeaderText]);
+      
     return(
         <div>
             <IncomingSales />
