4ffdbddc2501c693867ac8a8de9eef3cf75135d4
jang643 <jang643@naver.com>
2024-08-18 15:05:09 +0900

    Feat : Qr 목록 및 선택 전송 페이지 구현

--- src/components/setting/qrinfo/QrList.jsx
+++ src/components/setting/qrinfo/QrList.jsx
@@ -1,24 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import QrItem from "./QrItem";
 import { useQuery } from "@tanstack/react-query";
 import { getProductList } from "../../../api/incoming/incomingApi";
 
-const QrList = () => {
-	const [checkedCount, setCheckedCount] = useState(0);
-
+const QrList = ({ checkedItems, onCheckedChange }) => {
 	const { data: products, isPending, isError, error } = useQuery({
 		queryKey: ["productList"],
 		queryFn: () => getProductList("incoming"),
 	});
 
-	const handleCheckedChange = (isChecked) => {
-		setCheckedCount((prevCount) =>
-			isChecked ? prevCount + 1 : prevCount - 1
-		);
-	};
-
-	let buttonText = checkedCount
-		? `${checkedCount} 이메일로 보내기`
+	let buttonText = Object.values(checkedItems).filter(Boolean).length
+		? `${Object.values(checkedItems).filter(Boolean).length} 이메일로 보내기`
 		: "이메일로 보내기";
 
 	return (
@@ -28,9 +20,10 @@ const QrList = () => {
 				{isError && <div>Error: {error.message}</div>}
 				{products && products.map((product) => (
 					<QrItem
-						key={product.id}
+						key={product.productId}
 						product={product}
-						onCheckedChange={handleCheckedChange}
+						checked={checkedItems[product.productId] || false}
+						onCheckedChange={onCheckedChange}
 					/>
 				))}
 			</div>
