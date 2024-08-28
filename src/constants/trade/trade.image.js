17c58583c47acef650e67eb12a3bda9f06dd05d3
kimdodo97 <eh4536@naver.com>
2024-08-28 18:20:28 +0900

    Fix : 기본 브로커 이미지 변경

--- src/constants/trade/trade.image.js
+++ src/constants/trade/trade.image.js
@@ -1,7 +1,7 @@
 import searchIcon from "../../assets/common/search-icon.svg";
 import likeIcon from "../../assets/common/like-icon.svg";
 import unLikeIcon from "../../assets/common/unlike-icon.svg";
-import brokerLogo from "../../assets/common/broker-logo.svg";
+import brokerLogo from "../../assets/common/MainLogo.svg";
 
 
 export const tradeIcon = {
